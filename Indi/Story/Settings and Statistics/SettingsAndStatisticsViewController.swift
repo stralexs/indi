436e395191e5eb819e9436e344ddb4f1bcd5c1e6
Alexander <alexandersivko.swift@gmail.com>
2023-07-08 18:03:51 +0300

    Refactoring from MVC to MVVM: Settings and Statistics

--- Indi/Story/Settings and Statistics/SettingsAndStatisticsViewController.swift
+++ Indi/Story/Settings and Statistics/SettingsAndStatisticsViewController.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 class SettingsAndStatisticsViewController: UIViewController {
+    //MARK: - Variables
     @IBOutlet var nameTextField: UITextField!
-    
     @IBOutlet var leftAvatarButton: UIButton!
     @IBOutlet var rightAvatarButton: UIButton!
     @IBOutlet var avatarBackground: UIView!
@@ -21,24 +21,24 @@ class SettingsAndStatisticsViewController: UIViewController {
     @IBOutlet var rightAvatarImageLeading: NSLayoutConstraint!
     @IBOutlet var middleAvatarImageLeading: NSLayoutConstraint!
     @IBOutlet var middleAvatarImageTrailing: NSLayoutConstraint!
-    
     @IBOutlet var settingsBackground: UIView!
     @IBOutlet var statisticsBackground: UIView!
-    
     @IBOutlet var resetAchievementsSwitch: UISwitch!
     @IBOutlet var resetAchievementsBackground: UIView!
     @IBOutlet var statisticsPageControl: UIPageControl!
     
     private var statisticsScrollView = UIScrollView()
+    private var viewModel = SettingsAndStatisticsViewModel()
     
-    private let settingsModel = SettingsAndStatisticsModel()
-    private lazy var isUserClickedToChangeAvatar: Bool = false
-    
+    //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        addTapGesture()
         tuneUI()
+        createStatisticsPageControlAndScrollView()
     }
     
+    //MARK: - Private Methods
     private func tuneUI() {
         let buttonAttributes: [NSAttributedString.Key: Any] = [
             .foregroundColor: UIColor.indiMainYellow,
@@ -62,12 +62,14 @@ class SettingsAndStatisticsViewController: UIViewController {
         nameTextField.text = UserDataManager.shared.getUserName()
         nameTextField.returnKeyType = .done
         nameTextField.delegate = self
-        
+    }
+    
+    private func createStatisticsPageControlAndScrollView() {
         statisticsPageControl.currentPageIndicatorTintColor = UIColor.indiSaturatedPink
         statisticsPageControl.tintColor = UIColor.indiLightPink
 
         statisticsScrollView.backgroundColor = UIColor.clear
-        statisticsScrollView.contentSize = CGSize(width: Double(UIScreen.main.bounds.width) * Double(settingsModel.getUserStatisticsCount()), height: statisticsScrollView.frame.height)
+        statisticsScrollView.contentSize = CGSize(width: Double(UIScreen.main.bounds.width) * Double(viewModel.userStatisticsCount), height: statisticsScrollView.frame.height)
         statisticsScrollView.isPagingEnabled = true
         statisticsScrollView.showsHorizontalScrollIndicator = false
         statisticsScrollView.delegate = self
@@ -83,11 +85,34 @@ class SettingsAndStatisticsViewController: UIViewController {
             statisticsScrollView.bottomAnchor.constraint(equalTo: statisticsPageControl.topAnchor)
         ])
 
-        createContentForScrollView(for: settingsModel.getUserStatisticsInfo(for: 0), and: 0)
-        createContentForScrollView(for: settingsModel.getUserStatisticsInfo(for: 1), and: 1)
-        createContentForScrollView(for: settingsModel.getUserStatisticsInfo(for: 2), and: 2)
-        createContentForScrollView(for: settingsModel.getUserStatisticsInfo(for: 3), and: 3)
+        createContentForScrollView(for: viewModel.getUserStatisticsInfo(for: 0), and: 0)
+        createContentForScrollView(for: viewModel.getUserStatisticsInfo(for: 1), and: 1)
+        createContentForScrollView(for: viewModel.getUserStatisticsInfo(for: 2), and: 2)
+        createContentForScrollView(for: viewModel.getUserStatisticsInfo(for: 3), and: 3)
         
+        func createContentForScrollView(for userInfo: (String, String), and position: CGFloat) {
+            let statisticsDescription = UILabel()
+            statisticsDescription.text = userInfo.0
+            statisticsDescription.font = UIFont(name: "GTWalsheimPro-Regular", size: 20)
+            statisticsDescription.textAlignment = .center
+            statisticsDescription.textColor = UIColor.indiMainBlue
+            
+            let screenWidth = UIScreen.main.bounds.width
+            statisticsDescription.frame = CGRect(x: screenWidth * position, y: 0, width: screenWidth, height: statisticsScrollView.frame.height * 0.3)
+            
+            let statisticsUserResult = UILabel()
+            statisticsUserResult.text = userInfo.1
+            statisticsUserResult.font = UIFont(name: "GTWalsheimPro-Regular", size: 85)
+            statisticsUserResult.textAlignment = .center
+            statisticsUserResult.textColor = UIColor.indiMainBlue
+            statisticsUserResult.frame = CGRect(x: screenWidth * position, y: statisticsDescription.frame.maxY, width: screenWidth, height: statisticsScrollView.frame.height * 0.7)
+
+            statisticsScrollView.addSubview(statisticsDescription)
+            statisticsScrollView.addSubview(statisticsUserResult)
+        }
+    }
+    
+    private func addTapGesture() {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         view.addGestureRecognizer(tapGesture)
     }
@@ -96,60 +121,43 @@ class SettingsAndStatisticsViewController: UIViewController {
         view.endEditing(true)
     }
     
-    private func createContentForScrollView(for userInfo: (String, String), and position: CGFloat) {
-        let statisticsDescription = UILabel()
-        statisticsDescription.text = userInfo.0
-        statisticsDescription.font = UIFont(name: "GTWalsheimPro-Regular", size: 20)
-        statisticsDescription.textAlignment = .center
-        statisticsDescription.textColor = UIColor.indiMainBlue
-        
-        let screenWidth = UIScreen.main.bounds.width
-        statisticsDescription.frame = CGRect(x: screenWidth * position, y: 0, width: screenWidth, height: statisticsScrollView.frame.height * 0.3)
-        
-        let statisticsUserResult = UILabel()
-        statisticsUserResult.text = userInfo.1
-        statisticsUserResult.font = UIFont(name: "GTWalsheimPro-Regular", size: 85)
-        statisticsUserResult.textAlignment = .center
-        statisticsUserResult.textColor = UIColor.indiMainBlue
-        statisticsUserResult.frame = CGRect(x: screenWidth * position, y: statisticsDescription.frame.maxY, width: screenWidth, height: statisticsScrollView.frame.height * 0.7)
-
-        statisticsScrollView.addSubview(statisticsDescription)
-        statisticsScrollView.addSubview(statisticsUserResult)
-    }
-    
-    @IBAction func changeAvatar(_ sender: UIButton) {
-        isUserClickedToChangeAvatar = true
+    @IBAction private func changeAvatar(_ sender: UIButton) {
+        viewModel.userClickedToChangeAvatar = true
         if sender == leftAvatarButton {
-            UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseInOut] , animations: { self.rightAvatarImageLeading.constant -= 130
+            UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseInOut] , animations: {
+                self.rightAvatarImageLeading.constant -= 130
                 self.middleAvatarImageLeading.constant -= 260
                 self.view.layoutIfNeeded()
-            }) { [self ]_ in
-                middleAvatarImage.image = settingsModel.avatarSelection(true, with: .middle)
-                rightAvatarImage.image = settingsModel.avatarSelection(true, with: .right)
-                leftAvatarImage.image = settingsModel.avatarSelection(true, with: .left)
-                middleAvatarImageLeading.constant += 260
-                rightAvatarImageLeading.constant += 130
+            }) { [weak self] _ in
+                let images = self?.viewModel.avatarSwipe(true)
+                self?.leftAvatarImage.image = images?.0
+                self?.middleAvatarImage.image = images?.1
+                self?.rightAvatarImage.image = images?.2
+                self?.middleAvatarImageLeading.constant += 260
+                self?.rightAvatarImageLeading.constant += 130
             }
         } else {
             UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseInOut] , animations: {
                 self.middleAvatarImageTrailing.constant -= 260
                 self.leftAvatarImageTrailing.constant -= 260
                 self.view.layoutIfNeeded()
-            }) { [self] _  in
-                middleAvatarImage.image = settingsModel.avatarSelection(false, with: .middle)
-                leftAvatarImage.image = settingsModel.avatarSelection(false, with: .left)
-                rightAvatarImage.image = settingsModel.avatarSelection(false, with: .right)
-                middleAvatarImageTrailing.constant += 260
-                leftAvatarImageTrailing.constant += 260
+            }) { [weak self] _ in
+                let images = self?.viewModel.avatarSwipe(false)
+                self?.leftAvatarImage.image = images?.0
+                self?.middleAvatarImage.image = images?.1
+                self?.rightAvatarImage.image = images?.2
+                self?.middleAvatarImageTrailing.constant += 260
+                self?.leftAvatarImageTrailing.constant += 260
             }
         }
     }
     
-    @IBAction func resetAchievementsSwitchIsToggled(_ sender: UISwitch) {
+    @IBAction private func resetAchievementsSwitchIsToggled(_ sender: UISwitch) {
         if sender.isOn {
             let alertController = UIAlertController(title: "Данное действие удалит ВСЕ ваши достижения и прохождение", message: "После нажатия кнопки 'Готово' данное действие отменить нельзя", preferredStyle: .alert)
             let cancelAction = UIAlertAction(title: "Отмена", style: .default) { _ in
                 sender.isOn = false
+                self.viewModel.resetAchievements = false
             }
             let applyAction = UIAlertAction(title: "Ок", style: .destructive)
             
@@ -157,66 +165,48 @@ class SettingsAndStatisticsViewController: UIViewController {
             alertController.addAction(applyAction)
             
             self.present(alertController, animated: true)
+            viewModel.resetAchievements = true
         }
     }
     
     @objc private func applyChangesButtonIsPressed() {
+        let applyChangesResult = viewModel.applyChanges(for: nameTextField.text ?? "", and: middleAvatarImage.image?.imageAsset?.value(forKey: "assetName") as! String)
+        
         let emptyNameAlert = UIAlertController(title: "Пожалуйста, введите имя", message: nil, preferredStyle: .alert)
         let tooLongNameAlert = UIAlertController(title: "Имя не может быть длиннее 15 символов", message: nil, preferredStyle: .alert)
-        let onlySpacesInNameAlert = UIAlertController(title: "Имя не может содержать только пробелы", message: nil, preferredStyle: .alert)
         
         let okAction = UIAlertAction(title: "Ок", style: .default)
         emptyNameAlert.addAction(okAction)
         tooLongNameAlert.addAction(okAction)
-        onlySpacesInNameAlert.addAction(okAction)
         
-        let nameWithoutSpaces = settingsModel.removingSpaces(for: nameTextField.text ?? "")
-        
-        var countOfChanges = 0
-        
-        if nameTextField.text == Optional("") {
+        if applyChangesResult == "Empty name" {
             self.present(emptyNameAlert, animated: true)
-        } else if nameTextField.text!.count > 15 {
+        } else if applyChangesResult == "Too long name" {
             self.present(tooLongNameAlert, animated: true)
-        } else if nameWithoutSpaces == "" {
-            self.present(onlySpacesInNameAlert, animated: true)
-        } else if UserDataManager.shared.getUserName() != nameWithoutSpaces {
-            UserDataManager.shared.saveUserName(for: nameWithoutSpaces)
-            countOfChanges += 1
-        }
-                
-        if resetAchievementsSwitch.isOn {
-            UserDataManager.shared.resetAchievements()
-            countOfChanges += 1
         }
         
-        let middleImageName = middleAvatarImage.image?.imageAsset?.value(forKey: "assetName") as! String
-        let currentUserAvatar = UserDataManager.shared.getUserAvatar()
-        if isUserClickedToChangeAvatar && middleImageName != currentUserAvatar {
-            UserDataManager.shared.saveUserAvatar(for: middleImageName)
-            countOfChanges += 1
-        }
-        
-        let alertController = UIAlertController(title: "Изменения успешно сохранены!", message: nil, preferredStyle: .alert)
+        let changesAppliedAlert = UIAlertController(title: "Изменения успешно сохранены!", message: nil, preferredStyle: .alert)
         let okayAction = UIAlertAction(title: "Ок", style: .default) { _ in
             self.navigationController?.popToRootViewController(animated: true)
         }
         
-        alertController.addAction(okayAction)
-        if countOfChanges != 0 {
-            self.present(alertController, animated: true)
+        changesAppliedAlert.addAction(okayAction)
+        if applyChangesResult == "Changes saved" {
+            self.present(changesAppliedAlert, animated: true)
         } else {
             self.navigationController?.popToRootViewController(animated: true)
         }
     }
 }
 
+//MARK: - ScrollView Delegate
 extension SettingsAndStatisticsViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         statisticsPageControl.currentPage = Int(statisticsScrollView.contentOffset.x / UIScreen.main.bounds.width)
     }
 }
 
+//MARK: - TextFieldDelegate
 extension SettingsAndStatisticsViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
---
+++ Indi/Story/Settings and Statistics/SettingsAndStatisticsViewModel.swift
@@ -0,0 +1,151 @@
+//
+//  SettingsAndStatisticsViewModel.swift
+//  Indi
+//
+//  Created by Alexander Sivko on 21.05.23.
+//
+
+import Foundation
+import UIKit
+
+class SettingsAndStatisticsViewModel {
+    //MARK: - Private Variables
+    private var defaultAvatars: [UIImage] = []
+    private var userStatistics: [String: String] = [
+        "Пройдено тестов": "\(UserDataManager.shared.getUserStatistics().tests)",
+        "Сдано экзаменов": "\(UserDataManager.shared.getUserStatistics().exams)",
+        "Количество правильных ответов": "\(UserDataManager.shared.getUserStatistics().correct)",
+        "Процент правильных ответов": "\(UserDataManager.shared.getUserStatistics().percentage)%"
+    ]
+
+    private var leftAvatarIndex = 3 {
+        didSet {
+            if leftAvatarIndex > 3 {
+                leftAvatarIndex = 0
+            }
+            if leftAvatarIndex < 0 {
+                leftAvatarIndex = 3
+            }
+        }
+    }
+    private var middleAvatarIndex = 0 {
+        didSet {
+            if middleAvatarIndex > 3 {
+                middleAvatarIndex = 0
+            }
+            if middleAvatarIndex < 0 {
+                middleAvatarIndex = 3
+            }
+        }
+    }
+    private var rightAvatarIndex = 1 {
+        didSet {
+            if rightAvatarIndex > 3 {
+                rightAvatarIndex = 0
+            }
+            if rightAvatarIndex < 0 {
+                rightAvatarIndex = 3
+            }
+        }
+    }
+    //MARK: - Public Variables
+    var userStatisticsCount: Int {
+        return userStatistics.count
+    }
+    var userClickedToChangeAvatar: Bool = false
+    var resetAchievements: Bool = false
+    
+    //MARK: - Private Method
+    private func createAvatars() -> [UIImage] {
+        var tempArr = [UIImage]()
+        
+        let cat = UIImage(named: "Cat_emoji")
+        let dog = UIImage(named: "Dog_emoji")
+        let man = UIImage(named: "Man_emoji")
+        let woman = UIImage(named: "Woman_emoji")
+        
+        tempArr.append(cat!)
+        tempArr.append(dog!)
+        tempArr.append(man!)
+        tempArr.append(woman!)
+        
+        return tempArr
+    }
+    
+    private func removingSpaces(for text: String) -> String {
+        var outputText = text
+        
+        while outputText.first == " " {
+            outputText.removeFirst()
+        }
+        while outputText.last == " " {
+            outputText.removeLast()
+        }
+        
+        return outputText
+    }
+    
+    //MARK: - Public Methods
+    func applyChanges(for nameTexFieldText: String, and middleAvatarImageName: String) -> String {
+        var output = ""
+        var countOfChanges = 0
+
+        let nameWithoutSpaces = removingSpaces(for: nameTexFieldText)
+        
+        if nameWithoutSpaces == "" {
+            output = "Empty name"
+        } else if nameWithoutSpaces.count > 15 {
+            output = "Too long name"
+        } else if UserDataManager.shared.getUserName() != nameWithoutSpaces {
+            UserDataManager.shared.saveUserName(for: nameWithoutSpaces)
+            countOfChanges += 1
+        }
+        
+        if resetAchievements {
+            UserDataManager.shared.resetAchievements()
+            countOfChanges += 1
+        }
+        
+        let currentUserAvatar = UserDataManager.shared.getUserAvatar()
+        if userClickedToChangeAvatar && middleAvatarImageName != currentUserAvatar {
+            UserDataManager.shared.saveUserAvatar(for: middleAvatarImageName)
+            countOfChanges += 1
+        }
+        
+        if countOfChanges != 0 {
+            output = "Changes saved"
+        }
+        
+        return output
+    }
+    
+    func getUserStatisticsInfo(for position: Int) -> (String, String) {
+        var count = 0
+        var output = ("", "")
+        for (key, value) in userStatistics {
+            if count == position {
+                output = (key, value)
+            }
+            count += 1
+        }
+        return output
+    }
+    
+    func avatarSwipe(_ isLeftButton: Bool) -> (UIImage, UIImage, UIImage) {
+        if isLeftButton {
+            leftAvatarIndex += 1
+            middleAvatarIndex += 1
+            rightAvatarIndex += 1
+        } else {
+            leftAvatarIndex -= 1
+            middleAvatarIndex -= 1
+            rightAvatarIndex -= 1
+        }
+        return (defaultAvatars[leftAvatarIndex], defaultAvatars[middleAvatarIndex], defaultAvatars[rightAvatarIndex])
+    }
+    
+    //MARK: - Initialization
+    init() {
+        defaultAvatars = createAvatars()
+    }
+}
