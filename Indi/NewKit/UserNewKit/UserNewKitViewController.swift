a3917f77b047c531b768449c7edcaff975932572
Alexander <alexandersivko.swift@gmail.com>
2023-11-03 15:36:47 +0300

    UserNewKit Alert Controllers refactoring

--- Indi/NewKit/UserNewKit/UserNewKitViewController.swift
+++ Indi/NewKit/UserNewKit/UserNewKitViewController.swift
@@ -29,7 +29,19 @@ final class UserNewKitViewController: UIViewController {
         reloadTableViewOfTrainingMode()
     }
     
-    //MARK: - Methods
+    // MARK: - Initialization
+    init?(coder: NSCoder, viewModel: UserNewKitViewModelData & UserNewKitViewModelLogic) {
+        self.viewModel = viewModel
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
+
+    // MARK: - Rx Setup
+extension UserNewKitViewController {
     private func setupBinders() {
         viewModel.newKitName.bind { self.newKitLabel.text = $0 }
         .disposed(by: disposeBag)
@@ -48,23 +60,15 @@ final class UserNewKitViewController: UIViewController {
             }
         .disposed(by: disposeBag)
     }
-    
+}
+
+    // MARK: - Functionality
+extension UserNewKitViewController {
     private func reloadTableViewOfTrainingMode() {
         NotificationCenter.default.post(name: Notification.Name(rawValue: String.reloadTableView), object: nil)
     }
     
-    // MARK: - Initialization
-    init?(coder: NSCoder, viewModel: UserNewKitViewModelData & UserNewKitViewModelLogic) {
-        self.viewModel = viewModel
-        super.init(coder: coder)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError()
-    }
-    
-    //MARK: - Alert Controllers
-    @IBAction func nameKitButtonIsPressed(_ sender: UIButton?) {
+    @IBAction private func nameKitButtonIsPressed(_ sender: UIButton?) {
         let newKitNameAlert = UIAlertController(title: "Введите название нового набора слов:", message: nil, preferredStyle: .alert)
         newKitNameAlert.addTextField() { $0.clearButtonMode = .whileEditing }
         
@@ -76,19 +80,13 @@ final class UserNewKitViewController: UIViewController {
             catch let error {
                 switch error as! KitNameError {
                 case .empty:
-                    let emptyTextFieldAlert = UIAlertController(title: "Пожалуйста, введите корректное название набора", message: nil, preferredStyle: .alert)
-                    let okAction = UIAlertAction(title: "Ок", style: .default) { _ in
+                    self.presentBasicAlert(title: "Пожалуйста, введите корректное название набора", message: nil, actions: [.okAction]) { _ in
                         self.present(newKitNameAlert, animated: true)
                     }
-                    emptyTextFieldAlert.addAction(okAction)
-                    self.present(emptyTextFieldAlert, animated: true)
                 case .tooLong:
-                    let extraLongNameAlert = UIAlertController(title: "Название набора не может быть длиннее 30 символов", message: nil, preferredStyle: .alert)
-                    let okAction = UIAlertAction(title: "Ок", style: .default) { _ in
+                    self.presentBasicAlert(title: "Название набора не может быть длиннее 30 символов", message: nil, actions: [.okAction]) { _ in
                         self.present(newKitNameAlert, animated: true)
                     }
-                    extraLongNameAlert.addAction(okAction)
-                    self.present(extraLongNameAlert, animated: true)
                 }
             }
         }
@@ -98,7 +96,7 @@ final class UserNewKitViewController: UIViewController {
         self.present(newKitNameAlert, animated: true)
     }
     
-    @IBAction func studyStageButtonIsPressed(_ sender: UIButton) {
+    @IBAction private func studyStageButtonIsPressed(_ sender: UIButton) {
         let newKitStudyStageAlert = UIAlertController(title: "Выберите стадию обучения, в которую нужно добавить набор", message: nil, preferredStyle: .actionSheet)
         let cancelAction = UIAlertAction(title: "Отмена", style: .cancel)
         
@@ -130,49 +128,34 @@ final class UserNewKitViewController: UIViewController {
         self.present(newKitStudyStageAlert, animated: true)
     }
     
-    @IBAction func addNewKit(_ sender: UIButton) {
+    @IBAction private func addNewKit(_ sender: UIButton) {
         do {
             try viewModel.createNewKit()
-            let kitCreatedAlert = UIAlertController(title: "Новый набор успешно создан!", message: "Удалить набор можно долгим нажатием", preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "Ок", style: .default) { [weak self] _ in
-                self?.view.window!.rootViewController?.dismiss(animated: true, completion: nil)
+            presentBasicAlert(title: "Новый набор успешно создан!", message: "Удалить набор можно долгим нажатием", actions: [.okAction]) { _ in
+                self.view.window!.rootViewController?.dismiss(animated: true, completion: nil)
             }
-            kitCreatedAlert.addAction(okAction)
-            self.present(kitCreatedAlert, animated: true)
         }
         catch let error {
             switch error as! KitCreationError {
             case .noQuestions:
-                let alert = UIAlertController(title: "В наборе должен быть хотя бы один вопрос", message: nil, preferredStyle: .alert)
-                let okAction = UIAlertAction(title: "Ок", style: .default)
-                alert.addAction(okAction)
-                self.present(alert, animated: true)
+                presentBasicAlert(title: "В наборе должен быть хотя бы один вопрос", message: nil, actions: [.okAction], completionHandler: nil)
             case .noKitName:
-                let alert = UIAlertController(title: "Пожалуйста, введите название набора слов", message: nil, preferredStyle: .alert)
-                let okAction = UIAlertAction(title: "Ок", style: .default)
-                alert.addAction(okAction)
-                self.present(alert, animated: true)
+                presentBasicAlert(title: "Пожалуйста, введите название набора слов", message: nil, actions: [.okAction], completionHandler: nil)
             case .noStudyStage:
-                let alert = UIAlertController(title: "Пожалуйста, выберите стадию обучения, в которую нужно добавить набор", message: nil, preferredStyle: .alert)
-                let okAction = UIAlertAction(title: "Ок", style: .default)
-                alert.addAction(okAction)
-                self.present(alert, animated: true)
+                presentBasicAlert(title: "Пожалуйста, выберите стадию обучения, в которую нужно добавить набор", message: nil, actions: [.okAction], completionHandler: nil)
             case .nameAlreadyExists:
-                let nameAlreadyExistsAlert = UIAlertController(title: "В выбранной стадии обучения уже существует набор с таким названием", message: "Выберите другое название", preferredStyle: .alert)
-                let okAction = UIAlertAction(title: "Ок", style: .default) { [weak self] _ in
-                    self?.nameKitButtonIsPressed(nil)
+                presentBasicAlert(title: "В выбранной стадии обучения уже существует набор с таким названием", message: "Выберите другое название", actions: [.okAction]) { _ in
+                    self.nameKitButtonIsPressed(nil)
                 }
-                nameAlreadyExistsAlert.addAction(okAction)
-                self.present(nameAlreadyExistsAlert, animated: true)
             }
         }
     }
     
-    @IBAction func cancelButtonIsPressed(_ sender: UIButton) {
+    @IBAction private func cancelButtonIsPressed(_ sender: UIButton) {
         self.view.window!.rootViewController?.dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func addQuestion(_ sender: UIButton) {
+    @IBAction private func addQuestion(_ sender: UIButton) {
         let alertController = UIAlertController(title: "Добавить новый вопрос", message: nil, preferredStyle: .alert)
         alertController.addTextField() {
             $0.placeholder = "Вопрос"
@@ -182,44 +165,35 @@ final class UserNewKitViewController: UIViewController {
             $0.placeholder = "Правильный ответ"
             $0.clearButtonMode = .whileEditing
         }
-        alertController.addTextField(){
+        alertController.addTextField() {
             $0.placeholder = "Неправильные через запятую"
             $0.clearButtonMode = .whileEditing
         }
         
         let cancelAction = UIAlertAction(title: "Отмена", style: .cancel)
-        let addAction = UIAlertAction(title: "Добавить", style: .default) { [weak self] _ in
+        let addAction = UIAlertAction(title: "Добавить", style: .default) { _ in
             do {
-                try self?.viewModel.createNewQuestion(alertController.textFields?[0].text ?? "",
+                try self.viewModel.createNewQuestion(alertController.textFields?[0].text ?? "",
                                                       alertController.textFields?[1].text ?? "",
                                                       alertController.textFields?[2].text ?? "")
             }
             catch let error  {
                 switch error as! CreateQuestionError {
                 case .emptyFields:
-                    let emptyTextFieldsAlert = UIAlertController(title: "Пожалуйста, заполняйте все поля", message: nil, preferredStyle: .alert)
-                    let okAction = UIAlertAction(title: "Ок", style: .default) { _ in
-                        self?.present(alertController, animated: true)
+                    self.presentBasicAlert(title: "Пожалуйста, заполняйте все поля", message: nil, actions: [.okAction]) { _ in
+                        self.present(alertController, animated: true)
                     }
-                    emptyTextFieldsAlert.addAction(okAction)
-                    self?.present(emptyTextFieldsAlert, animated: true)
                 case .tooManyIncorrect:
-                    let wrongIncorrectAnswersNumberAlert = UIAlertController(title: "Число неправильных ответов должно быть от одного до трёх", message: nil, preferredStyle: .alert)
-                    let okAction = UIAlertAction(title: "Ок", style: .default) { _ in
-                        self?.present(alertController, animated: true)
+                    self.presentBasicAlert(title: "Число неправильных ответов должно быть от одного до трёх", message: nil, actions: [.okAction]) { _ in
+                        self.present(alertController, animated: true)
                     }
-                    wrongIncorrectAnswersNumberAlert.addAction(okAction)
-                    self?.present(wrongIncorrectAnswersNumberAlert, animated: true)
                 case .incorrectContainsCorrect:
-                    let incorrectEqualsCorrectAlert = UIAlertController(title: "Неправильные ответы не могут содержать правильный ответ", message: nil, preferredStyle: .alert)
-                    let okAction = UIAlertAction(title: "Ок", style: .default) { _ in
-                        self?.present(alertController, animated: true)
+                    self.presentBasicAlert(title: "Неправильные ответы не могут содержать правильный ответ", message: nil, actions: [.okAction]) { _ in
+                        self.present(alertController, animated: true)
                     }
-                    incorrectEqualsCorrectAlert.addAction(okAction)
-                    self?.present(incorrectEqualsCorrectAlert, animated: true)
                 }
             }
-            self?.tableView.reloadData()
+            self.tableView.reloadData()
         }
         
         alertController.addAction(cancelAction)
---
+++ Indi/SupportingFiles/AlertController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewControllerAlertControllerExtension.swift
+//  Indi
+//
+//  Created by Alexander Sivko on 3.11.23.
+//
+
+import UIKit
+
+struct CustomAlertAction: Equatable {
+    let title: String
+    let style: UIAlertAction.Style
+    
+    static let okAction: CustomAlertAction = .init(title: "Oк")
+    static let cancelAction: CustomAlertAction = .init(title: "Отмена", style: .cancel)
+    static let doneAction: CustomAlertAction = .init(title: "Готово")
+    
+    init(title: String, style: UIAlertAction.Style = .default) {
+        self.title = title
+        self.style = style
+    }
+}
+
+extension UIViewController {
+    func presentBasicAlert(title: String, message: String?, alertStyle: UIAlertController.Style = .alert, actions: [CustomAlertAction], completionHandler: ((_ action: CustomAlertAction) -> Void)?) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: alertStyle)
+        
+        for action in actions {
+            let alertAction = UIAlertAction(title: action.title, style: action.style) { _ in
+                guard let completionHandler = completionHandler else { return }
+                completionHandler(action)
+            }
+            alertController.addAction(alertAction)
+        }
+        
+        self.present(alertController, animated: true)
+    }
+}
